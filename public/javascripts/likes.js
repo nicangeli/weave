32001d840a3675478a78548c29dca17d0ddfab09
Nicholas Angeli <nicangeli@gmail.com>
2013-09-05 23:50:28 +0100

    cleaning up likes page

--- public/javascripts/likes.js
+++ public/javascripts/likes.js
@@ -10,13 +10,14 @@ $(document).ready(function() {
 	// get the localstorage likes
 	var likes = localStorage.getObj("likes");
 	for(var i = 0; i < likes.length; i++) {
-		var img = "<img src=" + likes[i].imageUrl + ">",
+		var img = "<img src=" + likes[i].imageUrl + " style='width: 200px; height: 400px;'>",
 			header = "<h2>" + likes[i].shop + "</h2>",
 			paragraph = "<p>" + likes[i].price + "</h2>",
 			anchor = '<a href="' + likes[i].url + '">Buy Now</a>';
 
 		// grab the heading and start appending items after it
-		$("#reveal").append(img + header + paragraph + anchor);
+		var element = '<div class="row"><div class="col-md-4">' + img + '</div><div class="col-md-8">' + header + paragraph + anchor + '</div></div><hr>';
+		$("#reveal").append(element);
 	}
 
 	$("#playAgain").click(function(e) {
