3fb7a77a910160ad2210fec478d5612209a92ec5
Nicholas Angeli <nicangeli@gmail.com>
2013-09-05 18:41:21 +0100

    share page

--- public/javascripts/share.js
+++ public/javascripts/share.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
 
-	
+	$("#share").click(function(e) {
+		e.preventDefault();
+
+		var friendNames = [$("#friendName1").val(), $("#friendName2").val(), $("#friendName3").val()],
+			friendEmails = [$("#friendEmail1").val(), $("#friendEmail2").val(), $("#friendEmail3").val()];
+
+		$.ajax({
+		  type: "POST",
+		  url: "/share",
+		  data: {
+		  	"emailAddresses": friendEmails,
+		  	"names": friendNames
+		  },
+		  success: function() {
+		  	window.location = "/likes";
+		  }
+		});
+	});
+
+	$("#skip").click(function(e) {
+		e.preventDefault();
+		window.location = "/likes";
+	})
 
 
 });
