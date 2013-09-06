e337c3cd8a869eef17e1068de6d8bece2c083910
Nicholas Angeli <nicangeli@gmail.com>
2013-09-06 12:40:40 +0100

    hanger

---
+++ public/javascripts/all.js
@@ -0,0 +1,14 @@
+Storage.prototype.setObj = function(key, obj) {
+    return this.setItem(key, JSON.stringify(obj))
+}
+Storage.prototype.getObj = function(key) {
+    return JSON.parse(this.getItem(key))
+}
+
+$(document).ready(function() {
+	if(localStorage.getObj("likes") == null) {
+		$("a.hanger").hide();
+	} else {
+		$("a.hanger").text(localStorage.getObj("likes").length + " Hanger")
+	}
+})
