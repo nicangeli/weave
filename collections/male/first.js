150b75a90e4972405989182f5363ec5f3d47d0bf
Nicholas Angeli <nicangeli@gmail.com>
2013-09-06 15:21:45 +0100

    male tracks

---
+++ collections/male/first.js
@@ -0,0 +1,163 @@
+exports.data = {
+    "products": [
+        {
+            "url" : "http://www.asos.com/ASOS/ASOS-Slim-Chino/Prod/pgeproduct.aspx?iid=3009623&cid=4910&sh=0&pge=0&pgesize=36&sort=-1&clr=Moonlight+blue",
+            "price" : "£23.00",
+            "shop" : "ASOS", 
+            "imageUrl" : "/images/male_trousers/item1.jpg",
+            "brand": "ASOS",
+            "type": "Trousers"
+        },
+        {
+            "url" : "http://www.asos.com/PS-by-Paul-Smith/PS-Paul-Smith-Carson-Suede-Brogues/Prod/pgeproduct.aspx?iid=3046397&cid=11941&Rf-200=3&sh=0&pge=0&pgesize=36&sort=-1&clr=Blue",
+            "price" : "£175.00",
+            "shop" : "ASOS",
+            "imageUrl" : "/images/dan/item2.jpg",
+            "brand": "Paul Smith",
+            "type": "Shoes"
+        },
+        {  
+            "url" : "http://www.topman.com/en/tmuk/product/topman-picks-2063182/new-in-140500/indigo-flannel-long-sleeve-shirt-2154380?refinements=category~%5b1208134%7c207138%5d&bi=1&ps=20",
+            "price" : "£30.00", 
+            "shop" : "Topman", 
+            "imageUrl" : "/images/male_top/mt1.jpg",
+            "brand": "Topman",
+            "type": "Top"
+        },
+        {
+            "url" : "http://www.topman.com/en/tmuk/product/clothing-140502/mens-shirts-140515/indigo-flannel-long-sleeve-shirt-2154380?bi=1&ps=20", 
+            "price" : "Ã‚Â£30.00", 
+            "shop" : "Topman", 
+            "imageUrl" : "/images/male_top/mt2.jpg",
+            "brand": "Topman",
+            "type": "Top"
+        },
+        {
+            "url" : "http://www.asos.com/G-Star/G-Star-Chinos-Correct-Nova-3d-Slim-Tapered-Micro-Twill/Prod/pgeproduct.aspx?iid=3032794&cid=4910&sh=0&pge=0&pgesize=36&sort=-1&clr=Khaki",
+            "price" : "95.00",
+            "shop" : "ASOS",
+            "imageUrl" : "/images/male_trousers/item2.jpg",
+            "brand": "G-Star",
+            "type": "Trousers"
+        },
+        {
+            "url" : "http://www.mrporter.com/product/378535", 
+            "price" : "£380.00",
+            "shop" : "Mr Porter",
+            "imageUrl" : "/images/josh/josh19.jpg",
+            "brand": "Officine Creative",
+            "type": "Shoes"
+        },
+        {
+            "url" : "http://www.howies.co.uk/mens/clothing/shirts/pendre-navy.html", 
+            "price" : "£49.00", 
+            "shop" : "Howies", 
+            "imageUrl" : "/images/male_top/mt3.jpg",
+            "brand": "Howies",
+            "type": "Shirt"
+        },
+        {
+            "url" : "http://www.uniqlo.com/uk/store/goods/078821", 
+            "price" : "£19.99", 
+            "shop" : "UNIQLO", 
+            "imageUrl" : "/images/male_top/mt4.jpg",
+            "brand": "UNIQLO",
+            "type": "top"
+        },
+        {
+            "url" : "http://www.urbanoutfitters.co.uk/shore-leave-garment-dyed-chinos-in-navy/invt/5221441721004/", 
+            "price" : "£45.00", 
+            "shop" : "Urban Outfitters", 
+            "imageUrl" : "/images/male_trousers/item3.jpg",
+            "type": "Trousers",
+            "brand": "Shore Leave"
+        },
+        {
+            "url" : "http://www.mrporter.com/product/357212?cm_mmc=Email-_-WNG-_-210513-_-INTL", 
+            "price" : "£330.00", 
+            "shop" : "Mr Porter", 
+            "imageUrl" : "/images/dan/item9.jpg",
+            "brand": "AMI",
+            "type": "Shoes"
+        },
+        {
+            "url" : "http://www.uniqlo.com/uk/store/goods/078824003", 
+            "price" : "£19.90", 
+            "shop" : "UNIQLO", 
+            "imageUrl" : "/images/male_top/mt5.jpg",
+            "brand": "UNIQLO"
+        },
+        {
+            "url" : "http://www.uniqlo.com/uk/store/goods/078619", 
+            "price" : "£19.90", 
+            "shop" : "UNIQLO", 
+            "imageUrl" : "/images/male_top/mt6.jpg",
+            "brand": "UNIQLO",
+            "type": "Top"
+        },
+        {
+            "url" : "http://www.asos.com/Izzue/Izzue-Chinos-With-Contrast-Turn-Up/Prod/pgeproduct.aspx?iid=3101897&cid=4910&sh=0&pge=0&pgesize=36&sort=-1&clr=Navy",
+            "price" : "£75.00",
+            "shop" : "ASOS",
+            "imageUrl" : "/images/male_trousers/item4.jpg",
+            "brand": "Izzue",
+            "type": "Trousers"
+        },
+        {
+            "url" : "http://www.office.co.uk/view/product/office_catalog/1,70/1320950012", 
+            "price" : "£74.99", 
+            "shop" : "Office", 
+            "imageUrl" : "/images/josh/josh9.jpg",
+            "brand": "Poste",
+            "type": "Shoes"
+        },
+        {
+            "url" : "http://www.uniqlo.com/uk/store/goods/078812001", 
+            "price" : "£19.90", 
+            "shop" : "UNIQLO", 
+            "imageUrl" : "/images/male_top/mt7.jpg",
+            "brand": "UNIQLO",
+            "type": "Top"
+        },
+        {
+            "url" : "http://www.urbanoutfitters.co.uk/obey-font-fleece-sweatshirt/invt/5216441238828/", 
+            "price" : "£60.00",
+            "shop" : "Obey", 
+            "imageUrl" : "/images/male_top/mt8.jpg",
+            "brand": "Obey",
+            "type": "Top"
+        },
+        {
+            "url" : "http://www.urbanoutfitters.co.uk/adidas-tonal-sweat-pants-in-leopard-print/invt/5221442653000/", 
+            "price" : "£65.00",
+            "shop" : "Urban Outfitters", 
+            "imageUrl" : "/images/male_trousers/item5.jpg",
+            "brand": "Adidas",
+            "type": "Trousers"
+        },
+        {
+            "url" : "http://www.office.co.uk/view/product/office_catalog/1,70/1277200078",
+            "price" : "£99.99", 
+            "shop" : "Office", 
+            "imageUrl" : "/images/josh/josh2.jpg",
+            "brand": "Ted Baker",
+            "type": "Shoes"
+        },
+        {
+            "url" : "http://www.urbanoutfitters.co.uk/selected-homme-neal-oxford-shirt/invt/5212443331050/", 
+            "price" : "£40.00", 
+            "shop" : "Urban Outfitters", 
+            "imageUrl" : "/images/male_top/mt9.jpg",
+            "brand": "Selected Homme",
+            "type": "Shirt"
+        },
+        {
+            "url" : "http://www.urbanoutfitters.co.uk/anerkjendt-wash-out-tee-in-black/invt/5214440681003/", 
+            "price" : "£25.00", 
+            "shop" : "Urban Outfitters", 
+            "imageUrl" : "/images/male_top/mt10.jpg",
+            "brand": "Anerkjendt",
+            "type": "Top"
+        }
+    ]
+};
