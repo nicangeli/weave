e1d95b35ede22eb0f2ab52fa17ff08e00898aa2e
Nicholas Angeli <nicangeli@gmail.com>
2013-09-11 18:41:27 +0100

    thanks working

--- public/javascripts/friend.js
+++ public/javascripts/friend.js
@@ -7,7 +7,7 @@ Storage.prototype.getObj = function(key) {
 
 $(document).ready(function() {
 	
-	$(".collections").hide();
+	$(".collections, .thanks").hide();
 
 
 	$("#understand").click(function(e) {
@@ -19,4 +19,54 @@ $(document).ready(function() {
 		}
 	});
 
+	$(".like").click(function(e) {
+		e.preventDefault();
+		var element = $(this).attr('data-number'),
+			_id = $(this).attr('data-id'),
+			likes = localStorage.getObj(_id);
+
+		var tmp = {};
+			tmp.url = $(this).attr("data-url");
+			tmp.price = $(this).attr("data-price");
+			tmp.shop = $(this).attr("data-shop");
+			tmp.imageUrl = $(this).attr("data-imageUrl");
+			tmp.type = $(this).attr("data-type");
+			tmp.brand = $(this).attr("data-brand");
+		if(likes == null) {
+			likes = [tmp];
+			localStorage.setObj(_id, likes);
+		} else {
+			likes.push(tmp);
+			localStorage.setObj(_id, likes);
+		}
+		
+		// anything that has a data-number attr of element, hide it
+		$('[data-number=' + element + ']').hide();
+		changeProduct(element);
+	});
+
+	$(".dislike").click(function(e) {
+		e.preventDefault();
+		var element = $(this).attr('data-number');
+		$('[data-number=' + element + ']').hide();
+		changeProduct(element);
+	});
+
 });
+
+var changeProduct = function(currentProduct) {
+	var num = parseInt(currentProduct.split("product")[1]),
+		next = num + 1,
+		nextId = "product" + next;
+	if($('[data-number=' + nextId + ']').length == 0) {
+		//window.location = "/thanks";
+		// we have been through all of the products, show the thanks screens
+		//and email the originalOwner
+		$(".collections").hide();
+		$(".thanks").show();
+	} else {
+		$('[data-number=' + nextId + ']').each(function() {
+			$(this).show();
+		});
+	}
+};
