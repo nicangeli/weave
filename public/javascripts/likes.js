7ffd4029ef209f81ee7f3b638f1e27aae59d7414
brownie3003 <brownie3003@gmail.com>
2013-09-11 16:27:04 +0100

    Andy fixes the bug

--- public/javascripts/likes.js
+++ public/javascripts/likes.js
@@ -71,6 +71,8 @@ $(document).ready(function() {
 
 	$("#nameCollection").click(function(e){
 		if($("form")[0].checkValidity()) {
+			e.preventDefault();
+			console.log($("form")[0].checkValidity());
 			var collectionName = $("#collectionName").val();
 			var ownerName = $("#ownerName").val();
 
@@ -85,7 +87,7 @@ $(document).ready(function() {
 				}
 			};
 	
-				$.post("/share", data)
+			$.post("/share", data)
 				.done(function(data) {
 	  				$(".modal-title").text("Share your first Collection");
 					$(".modal-body").html("<p>Your Collection is now ready, share this link with people and they'll weave through it. We'll let you know what they like and dislike</p> <p>" + data + "</p> <p> Twitter & facebook buttons?</p>");
@@ -94,7 +96,7 @@ $(document).ready(function() {
 					alert("Oops... Something went wrong")
 			});
 		}
-	})
+	});
 
 	$("#feedback").click(function(e) {
 		e.preventDefault();
