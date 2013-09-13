c0cc2e005f213f9f8480424f33a29ea0418e3222
brownie3003 <brownie3003@gmail.com>
2013-09-13 15:16:23 +0100

    vogue styling

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -87,5 +87,5 @@ var changeProduct = function(currentProduct) {
 };
 
 var updateHanger = function() {
-	$("a.hanger").html("<img src='/images/coat-hanger-white.png', style='width: 18px'> My Collection (" + localStorage.getObj("likes").length + ")").show();
+	$("a.hanger").html("<img src='/images/coat-hanger.png', style='width: 18px'> My Collection (" + localStorage.getObj("likes").length + ")").show();
 }
