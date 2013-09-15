66498321e3d4a0704705b0849c981687e536f836
Nicholas Angeli <nicangeli@gmail.com>
2013-09-15 22:06:23 +0100

    lets you rejoin a collection mid way through

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
 
-	// hide all elements at the start, bar the first one
-	for(var i = 1; i < $(".collections").children().length; i++) {
-		$('[data-number="product' + i + '"]').hide();
+	var collection = $(".collections").attr("data-collectionName")
+	var howFarThrough;
+	if(localStorage.getItem(collection) == null) {
+		// hide all elements at the start, bar the first one
+		for(var i = 1; i < $(".collections").children().length; i++) {
+			$('[data-number="product' + i + '"]').hide();
+		}
+	} else {
+		howFarThrough = parseInt(localStorage.getItem(collection));
+		for(var i = 0; i < howFarThrough; i++) {
+			$('[data-number="product' + i + '"]').hide();
+		}
+		for(var i = howFarThrough+1; i < $(".collections").children().length; i++ ) {
+			$('[data-number="product' + i + '"]').hide();
+		}
 	}
+
+
 
 	$(".like").click(function(e) {
 		e.preventDefault();
