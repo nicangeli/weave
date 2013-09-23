0df832bc9d1d643696c4fcf45b95ce5fd46c4720
brownie3003 <brownie3003@gmail.com>
2013-09-23 20:26:44 +0100

    validation on signup

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -1,31 +1,38 @@
 $(document).ready(function() {
+	
+	// hide warning
+	$(".signupError").hide();
 
 	$("#peopleSignup").click(function(e) {
 		e.preventDefault();
-		console.log('tracked')
-		mixpanel.identify();
-		
-		mixpanel.people.set({
-			"$email" : $("#email").val(),
-			"name" : $("#name").val()
-		});
+		if($("form")[0].checkValidity()) {
+			console.log('tracked')
+			mixpanel.identify();
+			
+			mixpanel.people.set({
+				"$email" : $("#email").val(),
+				"name" : $("#name").val()
+			});
 
-		mixpanel.track("Create Account", {
-		}, function() {
-			var data = {
-				name: $("#name").val(),
-				email: $("#email").val(),
-				password: $("#password").val()
-			}
-			$.post("/signup", data)
-				.done(function(data) {
-					window.location = "/collections";
-				})
-				.fail(function() {
-					alert("Oops... Something went wrong");
-				});
-			}
-		);
+			mixpanel.track("Create Account", {
+			}, function() {
+				var data = {
+					name: $("#name").val(),
+					email: $("#email").val(),
+					password: $("#password").val()
+				}
+				$.post("/signup", data)
+					.done(function(data) {
+						window.location = "/collections";
+					})
+					.fail(function() {
+						alert("Oops... Something went wrong");
+					});
+				}
+			);
+		} else {
+			$(".signupError").fadeIn("slow");
+		}
 	});
 
 
