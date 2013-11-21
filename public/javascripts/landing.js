c8176a1c8d79dccbbf55b268ea468b5ab4af5ef9
brownie3003 <brownie3003@gmail.com>
2013-11-21 18:12:53 +0000

    final commit of the dead weave

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -1,19 +1,14 @@
 $(document).ready(function() {
-	$('.email').hide()
+	$('.message').hide()
 
-	$(".getApp").click(function(e){
+	$(".submitEmail").click(function(e){
 		e.preventDefault();
-		mixpanel.track("Download App Main");
-		$('.getApp').hide();
-		$('.email').show();
-	});
-
-	$(".submitEmail").click(function(){
 		email = $("input.landingForm").val();
 		console.log(email);
 		mixpanel.track("Email 4 Download", {"email" : email}, function() {
 			window.location="https://itunes.apple.com/gb/app/weave-fashion/id725215782?mt=8&ign-mpt=uo%3D4";
 		});
-
+		$('.email').hide();
+		$(".message").show();
 	})
 });
